e0a888d7a7e65fa709a424ce33de806a074c7fae
peterkrieger <peterkrieger7@gmail.com>
2016-10-08 13:06:32 +0200

    Random Befuehlen des GameFeldes

--- src/agi/gameoflife/GameArea.java
+++ src/agi/gameoflife/GameArea.java
@@ -2,10 +2,8 @@ package agi.gameoflife;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 import java.util.List;
-import java.util.ListIterator;
 
 /**
  * Created by peterkrieger on 08.10.16.
@@ -13,29 +11,51 @@ import java.util.ListIterator;
 public class GameArea implements Area{
 
     private boolean [][]matrix;
-
+    Random rand = new Random();
 
     public GameArea(int breite, int hoehe, boolean random, List<Point2D> liste){
         matrix = new boolean[breite][hoehe];
+
+        // Felder werde zufällig befühlt
         if(random){
+            befuehleFelderRandom(breite, hoehe);
+        }
+        else
+            befuehleFelderMitBenutzerEingaben(liste);
+    }
+
+    private void befuehleFelderMitBenutzerEingaben(List<Point2D> liste) {
+
+    }
+
+    private void befuehleFelderRandom(int breite, int hoehe) {
+
+        List<Point2D> randomList = new ArrayList<>();
+        Random anzahlFelder = new Random();
+        int felder = anzahlFelder.nextInt(breite*hoehe)+1;
+        int schleifenDurchlauf = felder;
+        while (schleifenDurchlauf > 0 ) {
+            //zufällige Punkt wird erstellt
             Point2D randomPoint = new Point2D() {
+
                 @Override
                 public double getX() {
-                    return 0;
+                    return rand.nextInt(felder) + 1;
                 }
 
                 @Override
                 public double getY() {
-                    return 0;
+                    return rand.nextInt(felder) + 1;
                 }
 
+                //brauchen nicht?
                 @Override
                 public void setLocation(double x, double y) {
 
                 }
             };
-            List<Point2D> randomList;
-            //randomList.add(0, null);
+            randomList.add(randomPoint);
+            schleifenDurchlauf--;
         }
     }
 
