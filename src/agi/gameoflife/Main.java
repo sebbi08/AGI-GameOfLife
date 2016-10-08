7fe048404eb1d060383497f086968fd94f245a0f
peterkrieger <peterkrieger7@gmail.com>
2016-10-08 10:27:18 +0200

    vom Peter

--- src/agi/gameoflife/Main.java
+++ src/agi/gameoflife/Main.java
@@ -3,6 +3,8 @@ package agi.gameoflife;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("HALLO WELT!!!!");
+        System.out.println("HALLO von Peter!!!!");
     }
 }
